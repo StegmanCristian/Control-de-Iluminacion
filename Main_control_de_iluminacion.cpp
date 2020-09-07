bd98e213a1bda3a940772dab51191647ca129306
StegmanCristian <68197604+StegmanCristian@users.noreply.github.com>
2020-09-07 16:09:13 -0300

    Add files via upload

--- Main_control_de_iluminacion.cpp
+++ Main_control_de_iluminacion.cpp
@@ -5,6 +5,11 @@ int main()
     intensidad config;
     estados estado = off;    //definimos el primer estado de la maquina
 	config = f_setting();
+	if(config.i_set==-1)
+	{
+		print("Hubo un error en la carga de los datos de configuracion");
+		return 0;
+	}//Si f_setting carga un -1 en i_set entonces hubo un error en la carga de la configuracion
 	printf("Se cargo la configuracion de seteo:\n i_set: %d \n delta_i: %d\n",config.i_set,config.delta_i);
 	while(1)
 	{
